d7792e3cbfeb9238acb719c784cbb201eac055d5
Estrada Bernard <estradabernard@Estradas-MBP.lan>
2024-04-07 23:44:58 -0500

    adding stdout

--- computer_architecture/cs_primer/ring/terminal_ring.js
+++ computer_architecture/cs_primer/ring/terminal_ring.js
@@ -11,6 +11,7 @@ function createBellSound(value){
   if (valueToNum === NaN) return;
   
   for (let i = 0; i < valueToNum; i++){
+    process.stdout.write();
   }  
 }
 process.stdin.on("keypress", createBellSound);
