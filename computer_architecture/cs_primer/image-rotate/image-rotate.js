6188352449b62b95512468a21ca9975e685d6fa2
EBERNARD2 <carneybernard23@gmail.com>
2024-04-13 22:14:51 -0500

    img

--- computer_architecture/cs_primer/image-rotate/image-rotate.js
+++ computer_architecture/cs_primer/image-rotate/image-rotate.js
@@ -1,26 +1,52 @@
 const fs = require('node:fs');
+const { Buffer } = require('node:buffer');
+
+function copyHeader(img, stop){
+  const header = Buffer.alloc(stop);
+  for (let i = 0; i < stop; i++){
+    header.writeUintLE(img[i], 0, 1);
+  }
+  return header;
+}
+
+function findRowSize(bitsPerPixel, width){
+  // read https://en.wikipedia.org/wiki/BMP_file_format#Pixel_storage
+  return Math.ceil(((bitsPerPixel * width) / 32) * 4);
+}
+
+function rotateImage(rowSize, imgSize, image, offset){
+  // to roate image we'll start at last row and iterate down
+  const rowsToBuild = Math.floor(imgSize / rowSize);
+  
+  const rotatedImage = Buffer.alloc(imgSize);
+
+  for (let i = rowsToBuild; i > 0; i--){
+    let currentRowIdx = (i * rowSize - rowSize) + offset;
+    const currentRowEnd = i * rowSize + offset;
+
+    while (currentRowIdx != currentRowEnd){
+      rotatedImage.writeUintLE(image[currentRowIdx], 0, 1);
+      currentRowIdx++; 
+    }
+  }
+  return rotatedImage;
+}
 
 const image = fs.readFileSync("./teapot.bmp")
 const start = image.readUIntLE(10, 4);
 const width = image.readUintLE(18,4);
-const height = image.readUintLE(22, 4);
-
-function copyHeader(img, stop){
-  const header = [];
-  for (let byte of img) header.push(byte);
-  return header;
-}
+const bitsPerPixel = image.readUintLE(28, 2);
+const imgSize = image.readIntLE(34,4);
+const rowSize = findRowSize(bitsPerPixel, width);
 
 // copy header
 const head = copyHeader(image, start);
 
-// function createMatrix()
-// turn image pixels to a matrix 
+const rotatedImageBody = rotateImage(rowSize, imgSize, image, start);
+
+const finalImage = Buffer.concat([head, rotatedImageBody]);
+
+fs.writeFileSync("./rotated_image.bmp", finalImage);
+console.log(finalImage.length);
 
 
-// rotate pixels 90 degress
-
-
-// write new orentation to new bmp file
-
-console.log(head);
---
+++ computer_architecture/csapp/chapter2/hw/2.78.s
@@ -0,0 +1,20 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 2
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	mov	w0, #0
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.globl	_divide_power2                  ; -- Begin function divide_power2
+	.p2align	2
+_divide_power2:                         ; @divide_power2
+	.cfi_startproc
+; %bb.0:
+	ret
+	.cfi_endproc
+                                        ; -- End function
+.subsections_via_symbols
