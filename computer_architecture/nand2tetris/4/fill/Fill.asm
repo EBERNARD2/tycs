5e5b277d61a85996b5d18f506934b4bd7a71d57a
EBERNARD2 <carneybernard23@gmail.com>
2024-05-19 17:38:38 -0500

    5

--- computer_architecture/nand2tetris/4/fill/Fill.asm
+++ computer_architecture/nand2tetris/4/fill/Fill.asm
@@ -11,42 +11,39 @@
 // screen starts at address RAM[16384] or just call screen 
 // 
 
-(INIT)
-  @8192 // this is the register that maps back to the end of the screeen 
-  D=A
-  @i
-  M=D
+(INIT)  	//initializes i - index that runs on the screen's pixels
+	@8192	 // 32 * 256 number of 16 bit pixel lines to cover the entire screen
+	D=A
+	@i                   //initializes the index variable to 8192, this is the remaining address left to color onscreen
+	M=D
 
-(LOOP)
-  @i
-  M=M-1
-  @INIT
-  M;JLT
-  // get value in keyboard
-  @KBD
-  D=M
-  // if value is zero turn screen white
-  @WHITE
-  D;JEQ
-  // if there is a value turn screen black
-  @BLACK
-  0;JMP
+(LOOP)	           //progresses the index backwards. 
+	@i
+	M=M-1
+	D=M
+	@INIT
+	D;JLT               //if index<0 - go to INDEX INITIALIZER to reset it
+	@KBD	            //loads the keyboard's address
+	D=M
+	@WHITE		        //if (Memory at keyboard address == 0) - meaning no key is pressed - go to WHITE, else go to BLACK
+	D;JEQ
+	@BLACK
+	0;JMP
 
-  (WHITE)
-    // grab correct register address
-    @SCREEN
-    D=A
-    @i
-    A=D+M
-    M=0
-    @LOOP
-    0;JMP
+(BLACK)             
+	@SCREEN            //loads the screen's first address - 16384 (0x4000)
+	D=A
+	@i
+	A=D+M              //adds the current index to the screen's first address in order to color the current set of 16 pixels
+	M=-1               //sets value in current address to -1, so that the whole word 1...1 (16bits long),  meaning - all 16 pixels will be "painted" black.
+	@LOOP              //jumps back to indexer in order to progress the index backwards.
+	0;JMP
 
-  (BLACK)
-    @SCREEN
-    D=A
-    @i
-    A=D+M
-    M=-1
-    @LOOP
-    0;JMP
+(WHITE)
+	@SCREEN            //loads the screen's first address - 16384 (0x4000)
+	D=A                
+	@i        
+	A=D+M              //adds the current index to the screen's first address in order to color the current set of 16 pixels
+	M=0                //sets value in current address to 0, so that the whole word will be 0....0 (16bits long), meaning - all 16 pixels will be "painted" white.
+	@LOOP           //jumps back to indexer in order to progress the index backwards.
+	0;JMP
