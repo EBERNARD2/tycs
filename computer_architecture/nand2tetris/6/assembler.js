4d9d06fa521a1d2f36f041082de337a571f1b294
EBERNARD2 <carneybernard23@gmail.com>
2024-05-22 18:37:35 -0500

    working on assembler

--- computer_architecture/nand2tetris/6/assembler.js
+++ computer_architecture/nand2tetris/6/assembler.js
@@ -7,13 +7,7 @@ if (process.argv.length === 2) {
     process.exit(1);
 }
 
-const fileName = process.argv[2];
 
-const hackCode = fs.readFileSync(`./${fileName}`).toString();
-
-const hasMoreLine = () => {};
-
-const advance = () => {};
 
 const instructionType = () => {};
 
@@ -25,4 +19,63 @@ const comp = () => {};
 
 const jump = () => {};
 
+ 
+class Assembler {
+  constructor(){
+    const fileName = process.argv[2];
+    this.hackCode = fs.readFileSync(`./${fileName}`).toString();
+    this.index = 0; 
+    this.currentInstruction = null;
+  }
 
+  advance(){
+    const line = [];
+    while(this.hackCode[this.index] != '\n'){
+      const comment = (this.hackCode[this.index] === '/') && this.hackCode[this.index+1] === '/';
+      const currentValue = this.hackCode[this.index];
+      if (comment){
+        this.skipLine();
+        continue;
+      } else {
+        if (currentValue != ' ') line.push(currentValue);
+      }
+      this.index++;
+    }
+    this.currentInstruction = line.join('');
+    return;
+  }
+
+  code(){
+  }
+  
+  hasMoreLines() {
+    return !(this.hackCode.length - 1 == this.index);
+  }
+
+  instructionType(){
+    // determines type of instruction
+  }
+
+  parser(){
+    while (this.hackCode[this.index]){
+      // if there are more lines advance
+      if (this.hasMoreLines()) {
+        this.advance();
+        console.log(this.currentInstruction);
+      }
+      this.index++;
+    }
+  }
+
+  skipLine(){
+    while(this.hackCode[this.index] != '\n'){
+      this.index++;
+    }
+    this.index++;
+    return;
+  }
+
+}
+
+const assembler = new Assembler();
+assembler.parser();
