a3df4c28eb32e51c60b90c0d5a6ae52d91f9410d
EBERNARD2 <carneybernard23@gmail.com>
2024-05-29 12:47:53 -0500

    add operations enum

--- computer_architecture/virtual_machine/lc3.c
+++ computer_architecture/virtual_machine/lc3.c
@@ -22,3 +22,23 @@ enum
 };
 
 u_int16_t reg[R_COUNT];
+
+enum 
+{
+  OP_BR, /* BRANCH OP*/
+  OP_ADD, /* ADD OP*/
+  OP_LD, /* LOAD OP*/
+  OP_ST, /* STORE OP*/
+  OP_JSR, /* JUMP REGISTER*/
+  OP_AND, /* AND OP*/
+  OP_LDR, /*LOAD REGISTER*/
+  OP_STR, /* STORE REGISTER*/
+  OP_RTI, /* unused */
+  OP_NOT, /* NOT OP*/
+  OP_LDI, /* LOAD INDIRECT OP*/
+  OP_STI, /* STORE INDIRECT OP*/
+  OP_JMP, /* JUMP OP*/
+  OP_RES, /* RESERVED */
+  OP_LEA, /* LOAD EFFECTIVE ADDRESS OP*/
+  OP_TRAP, /* execute trap */
+};
