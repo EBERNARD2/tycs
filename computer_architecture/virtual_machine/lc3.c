d12a52d372286bdd0b54b79c22d65ac88e5f6901
EBERNARD2 <carneybernard23@gmail.com>
2024-05-30 16:58:04 -0500

    progress on vm

--- computer_architecture/virtual_machine/lc3.c
+++ computer_architecture/virtual_machine/lc3.c
@@ -1,8 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <signal.h>
 
 #define MEMORY_MAX (1 << 16)
-uint16_t memory[MEMORY_MAX];
 
 enum 
 {
@@ -21,11 +21,9 @@ enum
 
 };
 
-u_int16_t reg[R_COUNT];
-
 enum 
 {
-  OP_BR, /* BRANCH OP*/
+  OP_BR = 0, /* BRANCH OP*/
   OP_ADD, /* ADD OP*/
   OP_LD, /* LOAD OP*/
   OP_ST, /* STORE OP*/
@@ -43,3 +41,76 @@ enum
   OP_TRAP, /* execute trap */
 };
 
+
+enum 
+{
+  FL_POS = 1 << 0,
+  FL_ZRO = 1 << 1,
+  FL_NEG = 1 << 2
+};
+
+
+uint16_t memory[MEMORY_MAX];
+uint16_t reg[R_COUNT];
+
+
+uint16_t sign_extend(uint16_t x, int bit_count){
+  // if it is a negative number (MSB is 1) than add additional 1s to keep number negative
+  if ((x >> bit_count - 1) & 1){
+    x |= (0xFFFF << bit_count); 
+  }
+
+  return x;
+}
+
+uint16_t mem_read(uint16_t address){
+  return memory[address];
+}
+
+void update_flags(uint16_t r){
+  if (reg[r] == 0){
+    reg[R_COND] = FL_ZRO;
+  } else if (reg[r] >> 15) { // if MSB is 1
+    reg[R_COND] == FL_NEG;
+  } else {
+    reg[R_COND] == FL_POS;
+  }
+}
+
+int main(int argc, const char* argv[]){
+  // load arguments
+
+  // setup
+  if (argc < 2){
+    pritnt('lc3 [file] ...\n');
+    exit(2);
+  }
+
+  for (int j = 1; j < argc; ++j){
+    //read image 
+  }
+
+  enum { PC_START = 0x3000 };
+  reg[R_PC] = PC_START;
+
+  int running = 1;
+
+  while(running){
+
+    // read instruction from memmory
+    uint16_t instruction = mem_read(reg[R_PC]++);
+    // get the operation 
+    u_int16_t op = instruction >> 12;
+
+    switch(op){
+      case OP_ADD:
+        
+      break;
+
+      case OP_AND:
+
+      break;
+    }
+
+  }
+}
