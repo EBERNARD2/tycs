e75ea4bdebfc21bd94a76e6aef0ccb386f7bc655
EBERNARD2 <carneybernard23@gmail.com>
2024-06-02 18:33:13 -0500

    has more lines / advance functionality

--- computer_architecture/nand2tetris/7/vm.js
+++ computer_architecture/nand2tetris/7/vm.js
@@ -1,3 +1,5 @@
+const fs = require('node:fs');
+
 /* 
   Parser Module
   - Constructor takes input file / stream 
@@ -9,23 +11,47 @@
 
 */
 class Parser {
-  constructor(filePath){
-    const hasVmFileType = filePath.contains('.vm');
+  constructor(){
+    
+    const filePath = process.argv[2];
+    const hasVmFileType = filePath.includes('.vm');
 
-    if (!filePath || !hasVmFileType){
+    if (!filePath || !hasVmFileType || process.argv.length < 2){
+      if(process.argv.length < 2) console.error('Please add file to parse');
       if (!filePath)  console.error('Please add file to parse. Pass file path in when initializing new Parser');
       if (!hasVmFileType) console.error('Must have a .vm file extension');
 
       process.exit(1);
     }
- 
+    this.file = fs.readFileSync(filePath).toString();
+    this.lines = this.calculateLines();
     this.currentInstruction = null;
     this.currentInsructionType = null;
+    this.currentCommand = null;
+    this.index;
   }
 
-  hasMoreLines(){}
+  hasMoreLines(){
+    // a file has more lines if the current index is not the length of the file
+    return this.lines > 0 ? true : false;
+  }
 
-  advance(){}
+  advance(){
+    this.currentCommand = '';
+
+
+  }
+
+  calculateLines(){
+    let lines  = 0;
+    let index = 0;
+    
+    while(index != this.file.length){
+      if(this.file[index] === '\n') lines++;
+      index++
+    }
+    return lines;
+  }
 
   commandType(){}
 
@@ -34,3 +60,5 @@ class Parser {
   arg2(){}
 }
 
+
+const parse = new Parser('')
