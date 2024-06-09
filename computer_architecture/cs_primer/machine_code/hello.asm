c047c9925597d8a90131cf8d0bf2c34211b1beb1
EBERNARD2 <carneybernard23@gmail.com>
2024-06-09 15:59:31 -0500

    try again after downloading rosetta2

--- computer_architecture/cs_primer/machine_code/hello.asm
+++ computer_architecture/cs_primer/machine_code/hello.asm
@@ -1,21 +1,21 @@
 ; ----------------------------------------------------------------------------------------
-; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+; Writes "Hello, World" to the console using only system calls. Runs on 64-bit macOS only.
 ; To assemble and run:
 ;
-;     nasm -felf64 hello.asm && ld hello.o && ./a.out
+;     nasm -fmacho64 hello.asm && ld hello.o && ./a.out
 ; ----------------------------------------------------------------------------------------
 
-        global  start
+          global    _start
 
-        section .text
-start:
-        mov      rax, 0x02000004
-        mov      rdi, 1
-        mov      rsi, message
-        mov      rdx, 13
-        syscall
-        mov       rax, 0x02000001
-        xor      rdi, rdi
-        syscall
+          section   .text
+_start:    mov       rax, 0x02000004         ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 0x02000001         ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-message: db      "Hello, World", 10
+          section   .data
+message:  db        "Hello, World", 10      ; note the newline at the end
