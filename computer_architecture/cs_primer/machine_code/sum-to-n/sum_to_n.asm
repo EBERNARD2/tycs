60c69c7a66a5b39dbee2d0af5eed4f07bed8b153
EBERNARD2 <carneybernard23@gmail.com>
2024-06-10 11:40:23 -0500

    sum to n assembly

--- computer_architecture/cs_primer/machine_code/sum-to-n/sum_to_n.asm
+++ computer_architecture/cs_primer/machine_code/sum-to-n/sum_to_n.asm
@@ -1,4 +1,17 @@
 section .text
 global sum_to_n
 sum_to_n:
+	xor rax, rax
+	mov rsi, rdi
+	cmp rsi, 0 
+	jg  loop
+	jmp done
+
+loop: 
+	add rax, rsi
+	sub rsi, 1
+	cmp rsi, 0
+	jg loop
+
+done:
 	ret
