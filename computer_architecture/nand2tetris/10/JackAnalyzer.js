434bf02fc38fc13b8332247949d42807166baaed
EBERNARD2 <carneybernard23@gmail.com>
2024-06-19 12:33:09 -0500

    read inputFile

--- computer_architecture/nand2tetris/10/JackAnalyzer.js
+++ computer_architecture/nand2tetris/10/JackAnalyzer.js
@@ -26,6 +26,9 @@ const fs = require('node:fs');
 */
 
 
+
+// ./Path/To/File.js or /Usr/name/folder/here/file.js
+
 // Determine whether or not input string is a directory
 
 const getJackFiles = (inputFilePath) => {
@@ -33,18 +36,48 @@ const getJackFiles = (inputFilePath) => {
   // Break string up where instances of char "." occur in string
   const splitPath = inputFilePath.split(".");
 
-  // check if input path is a file path 
+  // Determine whether or not the input is a file path or directory 
+  // need to account for both relative and absolute paths
+  const isDir = `.${splitPath[1]}` === inputFilePath || splitPath[0] === inputFilePath;
+  if (!isDir){
     // if it is a file then make sure it is a jack file
+    const validFile = splitPath[splitPath.length - 1] === "jack";
 
+    if (!validFile){
+      console.error('Must enter a valid jack file extension');
+      process.exit(1);
+    }
+
+    console.log('Valid jack file');
     // if it is not a jack file throw an error and exit program
+     
+  } else {
+    // If not a file path try to fetch directory
+    try {
+      const dir = fs.readdirSync(inputFilePath);
 
+      // if successful log out all files in directory
+      for(const file of dir){
+        console.log(file);
+      }
 
-  // If not a file path try to fetch directory
-    // if successful log out all files in directory
+    } catch(err){
+      // if not a valid directory on machine throw an error 
 
-    // if not a valid directory on machine throw an error 
+      console.error(`Error: Not a valid file path`);
+      console.error(err);
+      process.exit(1);
+    }
 
+  }
+ 
 };
+
+
+
+getJackFiles(process.argv[2]);
+
+
 
 
 
