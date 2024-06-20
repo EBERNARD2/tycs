82e55f28781154ac5d2e37664080ca2a4a07c7f7
EBERNARD2 <carneybernard23@gmail.com>
2024-06-19 20:26:20 -0500

    change some values to private variables

--- computer_architecture/nand2tetris/10/JackTokenizer.js
+++ computer_architecture/nand2tetris/10/JackTokenizer.js
@@ -12,32 +12,37 @@ module.exports = class JackTokenizer {
     const fileLines = currentFile.toString().split('\n');
 
     this.file = fileLines.map((line) => line.trim());
-    this.currentIndex = 0;
     this.currentToken = null;
   }
 
+  #currentLine = this.file[this.#currentLineIndex];
+  #currentLineIndex = 0;
+
   hasMoreTokens(){
-    if (this.currentIndex != this.file.length) {
+    if (this.#currentLineIndex != this.file.length) {
       return true;
     }
 
     return false;
   }
 
+  // advance
+
+  getNextValidLine
+
   advance(){
 
     // Skip invalid lines until we find a line to process
-    let currentLine = this.file[this.currentIndex];
+    let currentLine = this.file[this.#currentLineIndex];
     let comment = currentLine.slice(0,2);
 
     while (COMMENT_SYMBOLS.includes(comment) || currentLine === BLANK_LINE) {
-      this.currentIndex++;
-      currentLine = this.file[this.currentIndex];
+      this.#currentLineIndex++;
+      currentLine = this.file[this.#currentLineIndex];
       comment = currentLine.slice(0,2);
     }
 
-    console.log(currentLine, 'found a line');
-    this.currentIndex++;
+    this.#currentLineIndex++;
   }
 
   tokenType(){}
