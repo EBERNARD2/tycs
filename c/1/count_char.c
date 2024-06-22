e3ad31823237cfeb4b3cc98193aa575df587ca23
EBERNARD2 <carneybernard23@gmail.com>
2024-06-22 17:36:00 -0500

    char count prog

---
+++ c/1/count_char.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define LENGTH 256
+
+int main(void)
+{
+  int chars[LENGTH] = {0}; // Save a slot for all LENGTH chars
+  int c, i; 
+
+  while ((c = getchar()) != EOF) {
+    chars[c - '0']++;
+  }
+
+
+  printf("Character Count from input stream: \n\n");
+
+  for (i = 0; i < LENGTH; i++) {
+    if (chars[i]){
+      printf("%c\t\t", i + '0');
+      for (int count = chars[i]; count > 0; count--)
+        printf("X");
+      printf("\t\t\t\t Appeared %c %d times\n", i + '0', chars[i]);
+    }
+  }
+
+
+  return 0;
+
+}
