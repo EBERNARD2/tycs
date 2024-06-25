863af85260c4e3df627b839f0b45fc1475b46b72
EBERNARD2 <carneybernard23@gmail.com>
2024-06-25 18:23:11 -0500

    hex convert

--- c/2/2_3.c
+++ c/2/2_3.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 
 int htoi(char s[]);
+
+
 int main(void)
 {
 
+  printf("%d\n", htoi("0x123456"));
+
+  return 0;
 }
+
+
+int htoi(char s[])
+{
+
+  int i, ms_hex, exp, len, magnitude, num, sum;
+
+  ms_hex = exp = 0;
+
+  if((s[0] == '0' && s[1] == 'x') || (s[0] == '0' && s[1] == 'x'))
+    ms_hex = 2;
+
+  len = strlen(s);
+
+
+  for ( i = len - 1; i >= ms_hex; i--) {
+    magnitude = pow(16, exp);
+
+    if (s[i] >= 48 && s[i] <= 57)
+      num = s[i] - 48;
+    else if (s[i] >= 65 && s[i] <= 70)
+      num = s[i] - 55;
+    else if (s[i] >= 97 && s[i] <= 102)
+      num = s[i] - 87;
+    else {
+      printf("Invalid hex number\n");
+      break;
+    }
+
+    sum += magnitude * num;
+    exp++;
+
+  }
+
+    return sum;
+  }
