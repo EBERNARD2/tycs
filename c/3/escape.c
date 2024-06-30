0317ce510e36037a53831cbabd969d20c050351a
EBERNARD2 <carneybernard23@gmail.com>
2024-06-30 14:46:23 -0500

    escape

--- c/3/escape.c
+++ c/3/escape.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
 
-void escape(char s[], char t[]);
+
+#define MAXLINE 1000
+
+char s[MAXLINE];
+int len;
+
+void escape(void);
 
 int main(void)
 {
+  while(get_text() > 0)
+    escape();
+    
+  return 0;
+}
+
+int get_text(void)
+{
+  extern int len;
+  extern char s[];
+
+  int c;
+
+  for(len = 0; (c = getchar()) != EOF && len < MAXLINE - 1; len++)
+    s[len++] = c;
+  
+  s[len] == '\0';
+
+  return len;
 
 }
 
-void escape(char s[], char t[])
+void escape(void)
 {
-  int c, i; 
+  char t[MAXLINE];
+  extern char s[];
+  extern int len;
+  int i, c;
 
-  while ((c = s[i]) != EOF) {
-    switch (c) {
-      case '\n': case '\t':
-        
+  i = 0;
+  
+  while(i < len - 1) {
+    c = s[i];
+    switch(c) {
+      case '\n':
+        t[i] = "newline";
+        break;
+      case '\t':
+        t[i] = 'tab';
+        break;
+      default:
+        t[i] = c;
         break;
     }
   }
