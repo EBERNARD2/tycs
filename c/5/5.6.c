3dd519292cd6c02b921fc660807db37d86dc7c58
EBERNARD2 <carneybernard23@gmail.com>
2024-07-04 18:40:19 -0500

    digit a to i

--- c/5/5.6.c
+++ c/5/5.6.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
+#include <ctype.h>
 
 void reverse(char*);
+int ato_i(char *);
 
 int main(void)
 {
   char s[] = "Reverse this string";
-
   reverse(s);
-
   printf("%s\n", s);
+
+  char dig[] = "5478";
+
+  printf("digit: %d\n", ato_i(dig));
+
   return 0;
 }
+
+int ato_i(char *s)
+{
+    int n = 0;
+
+    while (isdigit(*s))
+      n = 10 * n + (*s++ - '0');
+    
+    return n;
+}
+
 
 void reverse(char* s)
 {
