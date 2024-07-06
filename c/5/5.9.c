e648c722199d621f4fb17eb3ac27cea3aadb590e
EBERNARD2 <carneybernard23@gmail.com>
2024-07-05 19:34:38 -0500

    fix 5.9

--- c/5/5.9.c
+++ c/5/5.9.c
@@ -4,21 +4,20 @@
 int day_of_year(int, int, int);
 void month_day(int, int, int *, int *);
 
-static char daytab[][13] = {
-	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-};
+static char reg_year[] = 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+static char leap_year[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-// char * pdaytab =
+
+static char *pdaytab[2] = {reg_year, leap_year};
 
 int main(void)
 {
 	int yearday, month, day;
 
-	if ((yearday = day_of_year(1986, 3, 35)) < 0)
+	if ((yearday = day_of_year(2024, 3, 10)) < 0)
 		printf("Invalid input\n");
 	else
-		printf("day of the year: %i\n", yearday);
+		printf("day of the year: %d\n", yearday);
 	month_day(2020, 367, &month, &day);
 	if (!month || !day)
 		printf("Invalid input\n");
@@ -34,9 +33,15 @@ int day_of_year(int year, int month, int day)
 
 	if (year < 0 || month < 1 ||  month > 12 || day < 1 || day > 31)
 		return -1;
+
 	leap = (year % 4 == 0 && year % 100) || year % 400 == 0;
+
+  if (leap < 0 || leap > 1)
+    return -1;
+
 	for (i = 1; i < month; i++)
-		day += daytab[leap][i];
+		day += pdaytab[leap][i];
+
 	return day;
 }
 
@@ -50,8 +55,9 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
 		return;
 	}
 	leap = (year % 4 == 0 && year % 100) || year % 400 == 0;
-	for (i = 1; yearday > daytab[leap][i]; i++)
-		yearday -= daytab[leap][i];
+  int day = pdaytab[leap][i];
+	for (i = 1; yearday > day; i++)
+		yearday -= day;
 	*pmonth = i;
 	*pday = yearday;
 }
