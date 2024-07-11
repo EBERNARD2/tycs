0c7c5070e2fd0c291e4dbfefeb9952c06da944b9
EBERNARD2 <carneybernard23@gmail.com>
2024-07-10 23:47:55 -0400

    working on sorting portion

--- c/6/6.4.c
+++ c/6/6.4.c
@@ -54,6 +54,8 @@ int main(int argc, char *argv[])
 
   if (root != NULL)
     buildarray(allwords, root);
+  
+  wordnodesort(allwords, 0, len - 1);
 
 }
 
@@ -150,5 +152,27 @@ void buildarray(struct wordnode *dest[], struct wordnode *tree)
   }
  
 }
+
+
+struct wordnode *wordnodesort(struct wordnode *words[], int left, int right)
+{
+  int i, last;
+  void swap(char *v[], int i, int j);
+  if (left >= right) 
+    return;
+
+  swap(v, left, (left + right)/2);
+  last = left;
+  
+  for (i = left+1; i <= right; i++)
+    if (strcmp(v[i], v[left]) < 0)
+      swap(v, ++last, i);
+
+  swap(v, left, last);
+  qsort(v, left, last-1);
+  qsort(v, last+1, right);
+}
+
+
 
 
