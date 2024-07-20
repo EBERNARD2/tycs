a83485402d8febb929c22ea4c021d3afdb4e7eb9
EBERNARD2 <carneybernard23@gmail.com>
2024-07-20 10:31:44 -0500

    4

--- go/1/4.go
+++ go/1/4.go
@@ -10,30 +10,35 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	filesfound := make(map[string]string)
+	output := make(map[string]string)
 	files := os.Args[1:]
 
 	for _, filename := range files {
 		file, err := ioutil.ReadFile(filename)
+		
 		if err != nil {
-			fmt.Fprintf(Stderr, "dup %v\n", err)
+			fmt.Fprintf(os.Stderr, "dup %v\n", err)
 			continue
 		}
 
-		for _; line := range strings.Split(string(file), "\n") {
+		lineValuesFound := make(map[string]bool)
+
+
+		for _, line := range strings.Split(string(file), "\n") {
 			counts[line]++
-			filesfound[line] += filename
+			lineValuesFound[line] = true
+ 		}
+
+		for line := range lineValuesFound {
+			output[line] += filename + " "
 		}
-		
-		f.Close()
+
 	}
 
-
-}
-
-func countlines(f *os.File, counts map[string]int, filesfound map[string]string) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
+	for line, files := range output {
+		if counts[line] > 1 {
+			fmt.Printf("%s\t%s\n", line, files)
+		}
 	}
+
 }
