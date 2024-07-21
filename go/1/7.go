49acfe776b5c227e814d4eb9cd2415918cf1f27a
EBERNARD2 <carneybernard23@gmail.com>
2024-07-20 21:04:09 -0500

    working on 7

---
+++ go/1/7.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		for _, val := range
+		_, err := io.Copy(resp.Body, os.Stdout)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+	}
+}
