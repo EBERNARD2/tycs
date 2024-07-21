f06514bb090744e797336120f34c2e3a9d8d8acb
EBERNARD2 <carneybernard23@gmail.com>
2024-07-21 07:38:30 -0500

    8

---
+++ go/1/8.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		fullUrl := url
+
+		if !strings.HasPrefix(url, "http://") {
+			fullUrl = "http://" + fullUrl
+		} 
+
+		resp, err := http.Get(fullUrl)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		resp.Body.Close()
+	}
+}
