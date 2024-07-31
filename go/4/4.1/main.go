91029fe8a67f66cf903d465576fb996270768027
EBERNARD2 <carneybernard23@gmail.com>
2024-07-31 15:42:35 -0500

    4.1

--- go/4/4.1/main.go
+++ go/4/4.1/main.go
@@ -9,19 +9,31 @@ func main() {
 	c1 := sha256.Sum256([]byte("x"))
 	c2 := sha256.Sum256([]byte("X"))
 
-	fmt.Printf("c1: %x\nc2: %x", c1, c2)
+	fmt.Println(bitDiff(c1, c2))
 }
 
+func PopCount(x uint8) int {
+	i := 0
+	for x != 0 {
+		x = x & (x - 1)
+		i++
+	}
+	return i
+}
 
-func bitDiff(b1, b2 *[32]byte) int {
+func bitDiff(b1, b2 [32]byte) int {
 	count := 0
 
 	for i := 0; i < 32; i++ {
-		val1 := b1[i]
-		val2 := b2[i]
+		val1 := PopCount(b1[i])
+		val2 := PopCount(b2[i])
 
-		for val1 != 0 {
-			
-		}
+		if val1 > val2 {
+			count += val1 - val2
+		} else if val2 > val1 {
+			count += val2 - val1
+		} 
 	}
+
+	return count
 }
