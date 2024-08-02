00fe13f7636b6671739eb0c1bd9bd3191852a8c2
EBERNARD2 <carneybernard23@gmail.com>
2024-08-02 17:45:43 -0500

    working on adjacent

---
+++ go/4/4.5/main.go
@@ -0,0 +1,18 @@
+package main
+
+
+import "fmt"
+
+func main() {
+	s := []string{ "string", "string", "hi", "no", "goodbye"}
+	adjacentDups(s)
+
+}
+
+func adjacentDups(s []string) {
+	lastIdx := 0
+	for i, v := range s {
+		
+	}
+	fmt.Println(out)
+}
