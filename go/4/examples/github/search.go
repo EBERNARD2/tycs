ecdbd99e5197a1b5f04ef77b1f60d95e9ac3fdf1
EBERNARD2 <carneybernard23@gmail.com>
2024-08-06 20:21:06 -0500

    github

--- go/4/examples/github/search.go
+++ go/4/examples/github/search.go
@@ -9,5 +9,23 @@ import (
 )
 
 func SearchIssues(terms []string) (*IssuesSearchResult, error) {
-	
+	q := url.QueryEscape(strings.Join(terms, " "))
+	resp, err :=  http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOk {
+		resp.Body.Close()
+		return nil, fmt.Error("search query failed: %s", resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+
+	resp.Body.Close()
+	return &result, nil
 }
