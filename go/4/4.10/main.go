14e250776520a6ad9d1ec5e76ff612fd99e37aff
EBERNARD2 <carneybernard23@gmail.com>
2024-08-09 17:14:11 -0500

    4.10

--- go/4/4.10/main.go
+++ go/4/4.10/main.go
@@ -8,30 +8,40 @@ import (
 	"time"
 )
 
-type categories struct {
-	lessThanMonth []*Issue
-	lessThanYear  []*Issue
-	moreThanYear	[]*Issue
-} 
-
 func main() {
 	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+
+
+	lastMonth := time.Now().AddDate(0, -1, 0)
+	lastYear := time.Now().AddDate(-1, 0, 0)
+
+	fmt.Printf("Issues in the last month:\n\n")
+	for _, issue := range result.Items {
+		if lastMonth.Compare(issue.CreatedAt) == -1 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				issue.Number, issue.User.Login, issue.Title)
+		}
+	}
+
+	fmt.Printf("Issues less than a year:\n\n")
+	for _, issue := range result.Items {
+		if lastYear.Compare(issue.CreatedAt) == -1 && lastMonth.Compare(issue.CreatedAt) == 1 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				issue.Number, issue.User.Login, issue.Title)
+		}
+	}
+
+	fmt.Printf("Issues more than a year:\n\n")
+	for _, issue := range result.Items {
+		if lastYear.Compare(issue.CreatedAt) == 1 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				issue.Number, issue.User.Login, issue.Title)
+		}
 	}
 }
 
 
-func makeCategories() *categories {
-	var categorizedIssues  *categories
-	categorizedIssues = new(categories)
-
-	timeNow = time
-
-
-}
