f94710c3f08faa6e8d5dd5065856969e20e85e79
EBERNARD2 <carneybernard23@gmail.com>
2024-08-10 11:50:38 -0500

    4.12

---
+++ go/4/4.12A/comics.go
@@ -0,0 +1,12 @@
+package comic
+
+
+type Index struct {
+	Comics []*Comic
+}
+
+type Comic struct {
+	URL        string 
+	Transcript string
+	Img				 st
+}
---
+++ go/4/4.12A/main.go
@@ -0,0 +1,65 @@
+package main
+
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"encoding/json"
+	"os"
+)
+
+
+type ComicData struct {
+	Month 	   string `json:"month"`
+	Num 		   int		`json:"num"`
+	Year		   string	`json:"year"`
+	News 		   string `json:"news"`
+	SafeTitle  string `json:"safe_title"`
+	Transcript string `json:"tanscript"`
+	Alt 			 string `json:"alt"`
+	Img				 string `json:"img"`
+	Title 		 string `json:"title"`
+	Day 			 string `json:"day"`
+}
+
+const BaseUrl = "https://xkcd.com/info.0.json"
+
+func main() {
+
+	// fetch current number of comics 
+	currentNumOfComics := runSetup()
+	// for every request, create a comic
+}
+
+
+
+func runSetup() int {
+	// fetch current number of comics 
+	resp, err := http.Get(BaseUrl)
+
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		fmt.Errorf("Get request failed: %s", resp.Status)
+		os.Exit(1)
+	}
+
+	var comic ComicData
+
+	if err := json.NewDecoder(resp.Body).Decode(&comic); err != nil {
+		resp.Body.Close()
+		fmt.Errorf("Failed to parse body: %s", err)
+		os.Exit(1)
+	}
+
+	
+	currentComics := comic.Num
+	resp.Body.Close()
+	return currentComics
+}
+
