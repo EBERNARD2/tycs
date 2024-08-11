fca44a2ce36892b2a904efce9713c827e2612acb
EBERNARD2 <carneybernard23@gmail.com>
2024-08-11 11:20:47 -0500

    fix quant

--- computer_architecture/cs_primer/color-quantize/quantize.c
+++ computer_architecture/cs_primer/color-quantize/quantize.c
@@ -1,34 +1,15 @@
-#define RED0 0x00
-#define RED1 0x20
-#define RED2 0x40
-#define RED3 0x60
-#define RED4 0x80
-#define RED5 0xa0
-#define RED6 0xc0
-#define RED7 0xe0
-#define GREEN0 0x00
-#define GREEN1 0x04
-#define GREEN2 0x08
-#define GREEN3 0x0c
-#define GREEN4 0x10
-#define GREEN5 0x14
-#define GREEN6 0x18
-#define GREEN7 0x1c
-#define BLUE0 0x00
-#define BLUE1 0x01
-#define BLUE2 0x02
-#define BLUE3 0x03
+
 
 unsigned char quantize(unsigned char red, unsigned char green,
                        unsigned char blue) {
 
   unsigned char out = 0;
 
-  out |= (red << 5);
+  out |= (red >> 5) << 5;
 
-  out |= (green << 2);
+  out |= (green >> 5) << 2;
 
-  out |= blue;
+  out |= blue >> 6;
 
   return out;
 }
