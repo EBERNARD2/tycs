1cee1bb49f6794935868ff2a06442df02cc960d0
EBERNARD2 <carneybernard23@gmail.com>
2024-08-17 17:47:30 -0500

    cleanup 4.13 setup

---
+++ go/4/4.13/main.go
@@ -0,0 +1,40 @@
+package main
+
+
+import (
+	"fmt"
+	"os"
+	"encoding/json"
+)
+
+type Movie struct {
+	Title        string                   
+  Year         string   
+  Rated        string   
+  Released     string              
+  Runtime      string     
+  Genre        string       
+  Director     string                  
+  Writer       string                    
+  Actors       string                   
+  Plot         string                  
+  Language     string          
+  Country      string               
+  Awards       string    
+  Poster       string                   
+  Ratings      string  
+  Metascore    string       
+  imdbRating   string        
+  imdbVotes    string       
+  imdbID       string           
+  Type         string    
+  DVD          string 
+  BoxOffice    string       
+  Production   string        
+  Website      string     
+  Response     string                     
+} 
+
+func main() {
+
+}
