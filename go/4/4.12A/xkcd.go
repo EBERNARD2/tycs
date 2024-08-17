e0ba437f4df561f2b81230e97a7fd7340d8a250e
EBERNARD2 <carneybernard23@gmail.com>
2024-08-16 20:09:08 -0500

    unmarshal data

--- go/4/4.12A/xkcd.go
+++ go/4/4.12A/xkcd.go
@@ -4,7 +4,29 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"io"
+	"log"
+	"encoding/json"
 )
+
+type ComicData struct {
+	Month 	   string `json:"month"`
+	Num 		   int		`json:"num"`
+	Year		   string	`json:"year"`
+	News 		   string `json:"news"`
+	SafeTitle  string `json:"safe_title"`
+	Transcript string `json:"transcript"`
+	Alt 			 string `json:"alt"`
+	Img				 string `json:"img"`
+	Title 		 string `json:"title"`
+	Day 			 string `json:"day"`
+}
+
+type Comics struct {
+	ComicIndex map[int]ComicData
+	TranscriptIndexes map[string]int
+}
+
 
 func main() {
 	fmt.Println(len(os.Args))
@@ -14,5 +36,33 @@ func main() {
 	}
 
 	str := strings.Join(os.Args[1:], " ")
+	fmt.Println(str)
+	unmarshalData()
+
+}
+
+
+func unmarshalData() {
+	// get data from file
+	file, err := os.Open("index.txt")
+
+	if err != nil {
+		log.Fatalf("Failed to open file: %s", err)
+	}
+
+	b, err := io.ReadAll(file)
 	
+	if err != nil {
+		log.Fatalf("Reading file failed: %s", err)
+		os.Exit(1)
+	}
+
+	var data Comics 
+
+	if err := json.Unmarshal(b, &data); err != nil {
+		log.Fatalf("JSON unmarshalling failed: %s", err)
+	}
+
+	fmt.Println(data)
+
 }
