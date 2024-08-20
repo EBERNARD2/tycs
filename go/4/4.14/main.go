86b5b59033451a9e2cd716bc14e350523fde2fff
EBERNARD2 <carneybernard23@gmail.com>
2024-08-19 21:45:22 -0500

    initial server

---
+++ go/4/4.14/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+	"gopl.io/ch4/github"
+	"html/template"
+	"net/http"
+)
+
+var templ = template.Must(template.New("issueList").Parse(`
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='text-align: left'>
+	<th>#</th>
+	<th>State</th>
+	<th>User</th>
+	<th>Title</th>
+</tr>
+{{range.Items}}
+<tr>
+	<td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+	<td>State</td>
+	<td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+	<td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
+
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if templ.Execute(w, result); err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
