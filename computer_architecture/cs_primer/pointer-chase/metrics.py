91d8224d4893a90de7f2852d86a10c25b0911c55
EBERNARD2 <carneybernard23@gmail.com>
2024-08-21 20:40:31 -0500

    init performance increase

--- computer_architecture/cs_primer/pointer-chase/metrics.py
+++ computer_architecture/cs_primer/pointer-chase/metrics.py
@@ -4,30 +4,22 @@ import math
 import time
 
 
-class Address(object):
-    def __init__(self, address_line, zipcode):
-        self.address_line = address_line
-        self.zipcode = zipcode
-
-
-class DollarAmount(object):
-    def __init__(self, dollars, cents):
+class Payment(object):
+    def __init__(self, time, dollars, cents):
         self.dollars = dollars
         self.cents = cents
-
-
-class Payment(object):
-    def __init__(self, dollar_amount, time):
-        self.amount = dollar_amount
         self.time = time
+     
+
 
 
 class User(object):
-    def __init__(self, user_id, name, age, address, payments):
+    def __init__(self, user_id, name, age, address_line, zipcode ,payments):
         self.user_id = user_id
         self.name = name
         self.age = age
-        self.address = address
+        self.address_line = address_line
+        self.zipcode = zipcode
         self.payments = payments
 
 
@@ -44,7 +36,7 @@ def average_payment_amount(users):
     for u in users.values():
         count += len(u.payments)
         for p in u.payments:
-            amount += float(p.amount.dollars) + float(p.amount.cents) / 100
+            amount += float(p.dollars) + float(p.cents) / 100
     return amount / count
 
 
@@ -55,7 +47,7 @@ def stddev_payment_amount(users):
     for u in users.values():
         count += len(u.payments)
         for p in u.payments:
-            amount = float(p.amount.dollars) + float(p.amount.cents) / 100
+            amount = float(p.dollars) + float(p.cents) / 100
             diff = amount - mean
             squared_diffs += diff * diff
     return math.sqrt(squared_diffs / count)
@@ -63,16 +55,17 @@ def stddev_payment_amount(users):
 
 def load_data():
     users = {}
+    data = {}
     with open('users.csv') as f:
         for line in csv.reader(f):
             uid, name, age, address_line, zip_code = line
-            addr = Address(address_line, zip_code)
-            users[int(uid)] = User(int(uid), name, int(age), addr, [])
+            users[int(uid)] = User(int(uid), name, int(age), address_line, zip_code, [])
     with open('payments.csv') as f:
         for line in csv.reader(f):
             amount, timestamp, uid = line
             payment = Payment(
-                DollarAmount(dollars=int(amount)//100, cents=int(amount) % 100),
+                dollars=int(amount)//100, 
+                cents=int(amount) % 100,
                 time=datetime.datetime.fromisoformat(timestamp))
             users[int(uid)].payments.append(payment)
     return users
