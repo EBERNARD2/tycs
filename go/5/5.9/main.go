3e0762e5ba687cd6d077b18fe4ae9dacaeb6aa77
EBERNARD2 <carneybernard23@gmail.com>
2024-08-28 22:35:17 -0500

    working on 5.9

--- go/5/5.9/main.go
+++ go/5/5.9/main.go
@@ -4,13 +4,30 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"log"
 )
 
+const Pre string = "$foo"
 
 func main() {
+	if len(os.Args) == 1 {
+		log.Fatal("Usage: {filename} {string}")
+		os.Exit(1)
+	}
 
+	t := expand(os.Args[1], func(s string) { return fmt.Sprintf("hey there %s") })
+	fmt.Println(t)
 }
 
 func expand(s string, f func(string) string) string {
-	
+	w := ""
+	for _, c := range s {
+		if strings.HasPrefix(Pre, s) {
+			s = strings.CutPrefix(s, Pre)
+			w += f("you")  
+		} else {
+			w += c
+		}
+	}
+	return w
 }
