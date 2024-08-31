b3ba520f4c8e00abb5a26e943cbfafa60ef979ac
EBERNARD2 <carneybernard23@gmail.com>
2024-08-30 21:27:37 -0500

    setup

---
+++ go/5/5.12/main.go
@@ -0,0 +1,21 @@
+package main
+
+
+
+func main() {
+	
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
