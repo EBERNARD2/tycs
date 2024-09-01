92c02d7337093fb770e0f27bf337f36304bebaa1
EBERNARD2 <carneybernard23@gmail.com>
2024-09-01 16:11:25 -0500

    args

---
+++ bash/arguments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+function File {
+
+    echo $#
+}
+
+# Do not change anything
+if [ ! $# -lt 1 ]; then
+    File $*
+    exit 0
+fi
+
+# change here
+# here you can pass the arguments
+bash arguments.sh "Shell is fun"
