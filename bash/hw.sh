40a2f89b2f75849ede040be93bcb4c1d26cd1aa3
EBERNARD2 <carneybernard23@gmail.com>
2024-08-31 21:39:20 -0500

    test

--- bash/hw.sh
+++ bash/hw.sh
@@ -1,2 +1,5 @@
 #!/opt/homebrew/bin/bash
 echo 'Hello World!'
+
+
+# run file with chmod +x filename
---
+++ bash/test.sh
@@ -0,0 +1,3 @@
+FILELIST=`ls`
+FileWithTimeStamp=/tmp/my-dir/file_$(/bin/date +%Y-%m-%d).txt
+echo ${FileWithTimeStamp}
