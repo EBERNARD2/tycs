0ad7defb0b0453ae2b313e858ebcf50274526776
EBERNARD2 <carneybernard23@gmail.com>
2024-09-03 21:13:37 -0500

    complete

--- bash/string.sh
+++ bash/string.sh
@@ -3,10 +3,10 @@
 BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
 # write your code here
 ISAY=$BUFFETT
+echo "Before $ISAY"
 CHANGE1=${ISAY[@]/snow/foot}
 CHANGE2=${CHANGE1[@]// snow}
-CHANGE3=${CHANGE3[@]/finding/getting}
+CHANGE3=${CHANGE2[@]//finding/getting}
+ISAY=${CHANGE3%%w*}
+echo "After $ISAY"
 
-CHANGE4=${CHANGE3%%w*}
-echo $CHANGE4
-# ISAY=
