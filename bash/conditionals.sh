d18c11e9802bf6c69e45be5fe46987b7875cc0c4
EBERNARD2 <carneybernard23@gmail.com>
2024-09-03 21:17:34 -0500

    conditionals

---
+++ bash/conditionals.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NUMBER=16
+APPLES=16
+KING=LUIS
+
+if [ $NUMBER -gt 15 ] ; then
+  echo 1
+fi
+if [ $NUMBER -eq $APPLES ] ; then
+  echo 2
+fi
+if [[ ($APPLES -eq 12) || ("$KING" = "LUIS") ]] ; then
+  echo 3
+fi
+if [[ $(($NUMBER + $APPLES)) -le 32 ]] ; then
+  echo 4
+fi
