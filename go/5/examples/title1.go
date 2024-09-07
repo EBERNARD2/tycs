394306f782635a1a09b539d8a3464b6904c353b7
EBERNARD2 <carneybernard23@gmail.com>
2024-09-07 18:07:40 -0500

    title

---
+++ go/5/examples/title1.go
@@ -0,0 +1,34 @@
+package title
+
+import (
+	"net/http"
+	"strings"
+	"fmt"
+	"golang.org/x/net/html"
+)
+
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html") {
+		resp.Body.Close()
+		return fmt.Errorf("%s has type, not text/html", url, ct)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parsing %s HTML %v", url, err)
+	}
+
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && 
+			n.FirstChild != nil {
+				fmt.Println(n.FirstChild.Data)
+			}
+	}
+}
