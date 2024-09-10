a0be8de1fb752ae7646db23a6c19712d9e13bb7d
EBERNARD2 <carneybernard23@gmail.com>
2024-09-10 18:11:39 -0500

    w path

--- go/6/examples/geometry.go
+++ go/6/examples/geometry.go
@@ -3,6 +3,7 @@ package geometry
 import "math"
 
 type Point struct{X, Y, float64 }
+type Path []Point
 
 func Distance(p, q Point) float64 {
 	return math.Hypot(q.X - p.X, q.Y - p.Y)
@@ -11,4 +12,14 @@ func Distance(p, q Point) float64 {
 
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X - p.X, q.Y - p.Y)
+}
+
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])	
+		}
+	}
 }
