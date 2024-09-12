3958d1e3b24ade669e0b150803c803321d09986a
EBERNARD2 <carneybernard23@gmail.com>
2024-09-11 21:02:53 -0500

    trying to figure out string matching

--- bash/wiki.sh
+++ bash/wiki.sh
@@ -3,5 +3,8 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-DATA="$(curl -L https://en.wikipedia.org/wiki/$0)"
-echo $DATA
+DATA="$(curl -L https://en.wikipedia.org/wiki/$1)"
+# echo $DATA | grep mw-heading
+
+HEADINGS=$((read -a array <<< "$DATA" ))
+echo $HEADINGS
