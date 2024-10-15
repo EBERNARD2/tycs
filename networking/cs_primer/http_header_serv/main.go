d7b78f11f57e3a0fad13595d9224c7023a822fc3
EBERNARD2 <carneybernard23@gmail.com>
2024-10-15 11:34:26 -0500

    establish tcp connection with client

--- networking/cs_primer/http_header_serv/main.go
+++ networking/cs_primer/http_header_serv/main.go
@@ -8,27 +8,31 @@ import (
 
 var (
 	PORT    = 8888
-	ADDR    = []byte{127, 0, 0, 1}
+	ADDR    = [4]byte{127, 0, 0, 1}
 	BACKLOG = 20
 )
 
 func main() {
+	// Create TCP socket
 	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_IP)
 
 	if err != nil {
 		log.Fatalf("Error configuring socket. Please run program again")
 	}
 
+	// Set socket to localhost at port number
 	var sockIo syscall.SockaddrInet4
 	sockIo.Port = PORT
-	sockIo.Port = PORT
+	sockIo.Addr = ADDR
 
+	// Bind
 	err = syscall.Bind(fd, &sockIo)
 
 	if err != nil {
-		log.Fatalf("Error binding to port %s: %v", PORT, err)
+		log.Fatalf("Error binding to port %d: %v", PORT, err)
 	}
 
+	// Start listening on port only allowing 20 request queued at a time
 	err = syscall.Listen(fd, BACKLOG)
 
 	if err != nil {
@@ -38,11 +42,31 @@ func main() {
 	fmt.Printf("Listening on port %d...\n", PORT)
 
 	for {
-		err := syscall.Connect(fd, &sockIo)
+		// Establish TCP connection with client and create unique socket for two way communication w/ client
+		nfd, _, err := syscall.Accept(fd)
 
 		if err != nil {
-			fmt.Errorf("Error connecting to client: %v", err)
+			log.Fatalf("Error connecting to client: %v\n", err)
 		}
+
+		// Create buffer to read http header packet
+		buff := make([]byte, 1024)
+
+		// Read request headers
+		n, err := syscall.Read(nfd, buff)
+
+		if err != nil {
+			log.Fatalf("Error reading client msg: %v\n", err)
+		}
+
+		fmt.Println(string(buff[:n]))
+
+		// turn http headers into json so browser (the client) can render to page
+		t := []byte("this is a test")
+
+		syscall.Write(nfd, t)
+
+		syscall.Close(nfd)
 
 	}
 
