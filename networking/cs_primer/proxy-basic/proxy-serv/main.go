c5836c926000f7f4f3cc2d00fbfceffa67d091b8
EBERNARD2 <carneybernard23@gmail.com>
2024-10-27 13:02:45 -0500

    updates to fetch data properly

--- networking/cs_primer/proxy-basic/proxy-serv/main.go
+++ networking/cs_primer/proxy-basic/proxy-serv/main.go
@@ -22,14 +22,11 @@ func main() {
 		log.Fatalf("Unable to create proxy socket: %v\n", err)
 	}
 
-	clientSocket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_IP)
-
 	if err != nil {
 		log.Fatalf("Unable to create client socket: %v\n", err)
 	}
 
 	sockIO := syscall.SockaddrInet4{Port: PORT, Addr: ADDR}
-	staticIO := syscall.SockaddrInet4{Port: STATIC_PORT, Addr: ADDR}
 
 	if err = syscall.Bind(proxyFd, &sockIO); err != nil {
 		log.Fatalf("Unable to bind to socket: %v\n", err)
@@ -46,6 +43,11 @@ func main() {
 		sockIO.Addr[3],
 		sockIO.Port)
 
+	acceptClientConnections(proxyFd, sockIO)
+
+}
+
+func acceptClientConnections(proxyFd int, serverSocket syscall.SockaddrInet4) {
 	// Now we should listen an accept calls
 	for {
 		var httpMsg [4096]byte
@@ -64,59 +66,89 @@ func main() {
 
 		// create TCP connection with origin static server
 
-		// _, err = syscall.Write(clientSocket, []byte("GET / HTTP/1.0\r\n\r\n"))
-		err = syscall.Connect(clientSocket, &staticIO)
-
 		if err != nil {
 			log.Fatalf("Error connecting to Static server: %v", err)
-		}
+		} // Delete
 
-		// defer closing sockets
-		defer syscall.Close(clientSocket)
-		defer syscall.Close(connection)
+		res := fetchDataFromOrgin(serverSocket, httpMsg[:])
 
-		// read HTTP request
-		var req []byte
-
-		for _, headerLine := range bytes.Split(httpMsg[:], []byte{13, 10}) {
-			if strings.HasPrefix(string(headerLine), "Host:") {
-				// Transform host to be upstream server
-				req = append(req, []byte(fmt.Sprintf("Host: %d.%d.%d.%d:%d",
-					sockIO.Addr[0],
-					sockIO.Addr[1],
-					sockIO.Addr[2],
-					sockIO.Addr[3],
-					STATIC_PORT,
-				))...)
-
-			} else {
-				req = append(req, headerLine...)
-			}
-
-			// Add new line back into request
-			end := []byte{13, 10}
-			req = append(req, end...)
-		}
-
-		// Write request to socket
-		_, err = syscall.Write(clientSocket, req)
-
-		if err != nil {
-			log.Fatalf("Error writing to static server socket: %s\n", err)
-		}
-
-		var res [1024]byte
-		_, err = syscall.Read(clientSocket, res[:])
-
-		if err != nil {
-			log.Fatalf("Error reading from static server socket: %s\n", err)
-		}
-
+		// write response from origin server to client
 		_, err = syscall.Write(connection, res[:])
 
 		if err != nil {
 			log.Fatalf("Error reading from static server socket: %s\n", err)
 		}
 
+		syscall.Close(connection)
+
 	}
+
+}
+
+func fetchDataFromOrgin(socket syscall.SockaddrInet4, httpMsg []byte) []byte {
+	clientSocket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_IP)
+
+	if err != nil {
+		log.Fatalf("Unable to create client socket: %v\n", err)
+	}
+
+	staticIO := syscall.SockaddrInet4{Port: STATIC_PORT, Addr: ADDR}
+
+	// create TCP connection with origin static server
+	err = syscall.Connect(clientSocket, &staticIO)
+
+	if err != nil {
+		log.Fatalf("Error connecting to Static server: %v", err)
+	}
+
+	// read HTTP request
+	var req []byte
+
+	for _, headerLine := range bytes.Split(httpMsg[:], []byte{13, 10}) {
+		if strings.HasPrefix(string(headerLine), "Host:") {
+			// Transform host to be upstream server
+			req = append(req, []byte(fmt.Sprintf("Host: %d.%d.%d.%d:%d",
+				socket.Addr[0],
+				socket.Addr[1],
+				socket.Addr[2],
+				socket.Addr[3],
+				STATIC_PORT,
+			))...)
+
+		} else {
+			req = append(req, headerLine...)
+		}
+
+		// Add new line back into request
+		end := []byte{13, 10}
+		req = append(req, end...)
+	}
+
+	// Write request to socket
+	_, err = syscall.Write(clientSocket, req)
+
+	if err != nil {
+		log.Fatalf("Error writing to static server socket: %s\n", err)
+	}
+
+	buff := make([]byte, 4096)
+	var res []byte
+
+	for {
+		n, err := syscall.Read(clientSocket, buff)
+
+		if n == 0 {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error reading from static server socket: %s\n", err)
+		}
+
+		res = append(res, buff[:n]...)
+	}
+
+	syscall.Close(clientSocket)
+
+	return res[:]
 }
