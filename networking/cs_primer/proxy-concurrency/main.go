4c9354a1c7225ce89b46c61b453a6504a7eebf3c
EBERNARD2 <carneybernard23@gmail.com>
2024-11-06 18:51:20 -0600

    scrap module

--- networking/cs_primer/proxy-concurrency/main.go
+++ networking/cs_primer/proxy-concurrency/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
+	"slices"
+	"strings"
 	"syscall"
-
-	"proxy-concurrency/httpMessageBuilder"
 )
 
 var (
@@ -13,6 +14,17 @@ var (
 	OWN_PORT      = 8080
 	UPSTREAM_PORT = 3005
 )
+
+var acceptedMethods = []string{"GET", "POST", "PATCH", "DELETE"}
+var acceptedVersions = []string{"HTTP/1.0", "HTTP/1.1", "HTTP/2"}
+
+type HTTPMessage struct {
+	Method  string
+	Version string
+	Uri     string
+	Headers map[string]string
+	Body    []byte
+}
 
 func main() {
 	sock := createSocket()
@@ -41,14 +53,17 @@ func main() {
 			continue
 		}
 
-		req, err := httpMessageBuilder.BuildMessage(msg[:n])
+		req, err := BuildMessage(msg[:n])
 
 		if err != nil {
 			log.Printf("Error building http message: %v", err)
 			continue
 		}
 
-		fmt.Println(req)
+		fmt.Println(req.Method)
+		fmt.Println(req.Headers["Connection: "])
+		fmt.Print(req.Body)
+
 		go connectClientUpstream(clientSock, msg[:n], upstreamAddr)
 
 	}
@@ -138,4 +153,38 @@ func createSocket() int {
 		log.Fatal(err)
 	}
 	return sock
+}
+
+func BuildMessage(b []byte) (*HTTPMessage, error) {
+	bodyStartIndex := bytes.Index(b, []byte{0x0d, 0x0a, 0x0d, 0x0a})
+	parseRequest := bytes.Split(b[:bodyStartIndex], []byte{0x0d, 0x0a})
+	var message HTTPMessage
+
+	requestLine := bytes.Split(parseRequest[0], []byte{32})
+
+	message.Method = string(requestLine[0])
+	message.Uri = string(requestLine[1])
+	message.Version = string(requestLine[2])
+
+	if !slices.Contains(acceptedMethods, message.Method) {
+		return nil, fmt.Errorf("method not accepted: %s", message.Method)
+	}
+
+	if !slices.Contains(acceptedVersions, message.Version) {
+		return nil, fmt.Errorf("version not accepted: %s", message.Version)
+	}
+
+	// make headers
+	message.Headers = make(map[string]string)
+
+	for i := 1; i < len(parseRequest); i++ {
+		header := strings.Split(string(parseRequest[i]), ": ")
+		message.Headers[header[0]] = header[1]
+	}
+
+	// add body to http message body.. Skip CRLF
+	message.Body = b[bodyStartIndex+4:]
+
+	return &message, nil
+
 }
