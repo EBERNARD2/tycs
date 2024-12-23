24f1df282fc4a33b78e52821f211a3c9191baaaf
EBERNARD2 <carneybernard23@gmail.com>
2024-12-23 17:26:17 -0600

    refactoring to use select

--- networking/cs_primer/concurrency/concurrent.py
+++ networking/cs_primer/concurrency/concurrent.py
@@ -4,56 +4,108 @@ import select
 ADDR = ("0.0.0.0", 8000)
 UPSTREAM = ("127.0.0.1", 3005)
 
+
+MESSAGE_QUEUES = {}
+
+
 def log(err):
   print(err, file=sys.stderr)
 
 
 
+def main():
+  server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server_sock.setblocking(0)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(ADDR)
-sock.listen(20)
-log(f"Accepting connections on port {ADDR}")
+  server_sock.bind(ADDR)
+  server_sock.listen(20)
+  log(f"Accepting connections on port {ADDR}")
 
-while True:
-  try:
-    keepAlive = True
-    while keepAlive:
+  # Create sockets to read from 
+  inputSockets = [server_sock]
+  # Create sockets to write to
+  outputSockets = []
+  while True:
+  # create new select sockets
+    readable, writable, exceptions = select.select(inputSockets, outputSockets, server_sock)
+    
+    for sock in readable:
+      if sock is server_sock:
+        clientSock, clientAddr = server_sock.accept()
+        clientSock.setblocking(0) # Non blocking socket
+        log(f'New connection from {clientAddr}')
+        inputSockets.append(clientSock)
+        MESSAGE_QUEUES[sock] = []
+      else:
+        data = sock.recv(4096)
+        if data:
+          log(f'Recieved {data} from {clientAddr}')
+          keepAlive = keepAlive(data)
+          MESSAGE_QUEUES[sock].append(data)
 
-      clientSock, clientAddr = sock.accept()
-      log(f'New connection from {clientAddr}')
+          if sock not in outputSockets and keepAlive:
+            outputSockets.append(sock)
 
-      data = clientSock.recv(4096)
-      log(f'Recieved {data} from {clientAddr}')
-
-      for line in bytes(data).decode("utf-8").split("\r\n"):
-        if line.startswith("Connection: "):
-          connectionStatus = line.replace("Connection: ", "")
-          if connectionStatus != "keep-alive":
-            keepAlive = False
+          if not keepAlive:
+             inputSockets.remove(sock)
+             del MESSAGE_QUEUES[sock]
           
-      upstreamSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      upstreamSock.connect(UPSTREAM)
+          
+        # remove socket if it is not a persistent connection 
+    for sock in writable:
+      print()
 
-      log(f'Connected to upstream server')
-      upstreamSock.send(data)
-      log(f'Sent {len(data)}B upstream')
+      # check if we have a message to send to writable socket - IN QUEUE 
 
 
-      while True:
-        res = upstreamSock.recv(4096)
-        if not res:
-          break
-        clientSock.send(res)
-        log(f'Sent {len(data)}B to client')
+    try:
+      keepAlive = True
+      while keepAlive:
 
-  except ConnectionRefusedError:
-    clientSock.send(b'HTTP/1.1 502 Bad Gateway\r\n\r\n')
-    log('<- *    BAD GATEWAY')
+        clientSock, clientAddr = server_sock.accept()
+        log(f'New connection from {clientAddr}')
 
-  finally:
-    clientSock.close()
-    upstreamSock.close()
+        data = clientSock.recv(4096)
+        log(f'Recieved {data} from {clientAddr}')
 
-sock.close()
+        keepAlive = keepAlive(data)
+        upstreamSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        upstreamSock.connect(UPSTREAM)
+
+        log(f'Connected to upstream server')
+        upstreamSock.send(data)
+        log(f'Sent {len(data)}B upstream')
+
+
+        while True:
+          res = upstreamSock.recv(4096)
+          if not res:
+            break
+          clientSock.send(res)
+          log(f'Sent {len(data)}B to client')
+
+    except ConnectionRefusedError:
+      clientSock.send(b'HTTP/1.1 502 Bad Gateway\r\n\r\n')
+      log('<- *    BAD GATEWAY')
+
+    finally:
+      clientSock.close()
+      upstreamSock.close()
+    
+  server_sock.close()
+
+
+def keep_alive(data) -> bool:
+  for line in bytes(data).decode("utf-8").split("\r\n"):
+    if line.startswith("Connection: "):
+      connectionStatus = line.replace("Connection: ", "")
+      if connectionStatus != "keep-alive":
+        return False
+
+
+
+if __name__ == "__main__":
+  main()
+
+
 
