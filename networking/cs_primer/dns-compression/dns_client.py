9f45349c5535100f7042fd37414ecf26cf85cc0a
EBERNARD2 <carneybernard23@gmail.com>
2025-01-06 21:58:05 -0600

    fix bug

--- networking/cs_primer/dns-compression/dns_client.py
+++ networking/cs_primer/dns-compression/dns_client.py
@@ -6,11 +6,9 @@ import random
 GOOGLE_PUBLIC_DNS = ('8.8.8.8', 53)
 
 
-# rename to skip question
 def skip_name(res, i):
     while True:
         x = res[i]
-        # if this is a pointer to rname skip
         if x & 0b11000000:
             return i + 2
         if x == 0x00:
@@ -31,10 +29,57 @@ def parse_name(res, i):
         - Print Data with with name
 
     """
+    i = skip_name(res, i) # skip name in answer
+
     rtype, rclass, ttl, rdlength = struct.unpack('!HHIH', res[i:i+10])
-    ns = res[i+10:i+10+rdlength]
-    # get pointer name
-    pass
+    i += 10
+
+    # print name server
+
+    nsname = b''
+
+    while res[i] != 0xC0 or i > len(res):
+        nslen = res[i]
+        nsname += res[i+1:i+1+nslen]
+        i += nslen+1
+        if i >= len(res):
+            break
+        else:
+            nsname += b"."
+    
+    try:
+        # assert byte is a pointer
+        assert res[i] == 0xC0
+    except AssertionError:
+        # else we are out of range
+        return
+    
+
+    i += 1 
+    offset = int(res[i])
+    i += 1
+
+    domain_name = b''
+    # get name chunks until it terminates
+    while res[offset] != 0x00:
+        # get location
+        partlen = int(res[offset])
+        # Location will start with length of value so by pass that and get part
+        partname = res[offset + 1: offset + 1 + partlen]
+        # add part to domain name
+        domain_name += partname + b"."
+        # get next offset location
+        offset += partlen + 1
+
+        if res[offset] == 0xC0:
+            offset = int(res[offset + 1])
+        
+
+
+
+    print(nsname + domain_name)
+
+    return i
 
 if __name__ == '__main__':
     hostname = sys.argv[1]
@@ -46,7 +91,7 @@ if __name__ == '__main__':
         len(b).to_bytes(1, 'big') + b.encode('ascii')
         for b in hostname.split('.')) + b'\x00'
     query += qname
-    query += struct.pack('!HH', 2, 1) # NS Qtype
+    query += struct.pack('!HH', 2, 1) # Query NS Type
     s.sendto(query, GOOGLE_PUBLIC_DNS)
     # loop until we get the response to our answer
     while True:
@@ -63,53 +108,9 @@ if __name__ == '__main__':
     i = 12
     i = skip_name(res, i)  # skip name in question
     i += 4  # skip qtype and qclass
+
+    print("ANSWER SECTION:", end="\n\n")
+     
     # iterate over all answers
-    i = skip_name(res, i) # skip name in answer
-    rtype, rclass, ttl, rdlength = struct.unpack('!HHIH', res[i:i+10])
-    i += 10
-    print(res[i:i+rdlength])
-    
-    nslen = res[i]
-    print(res[i+1:i+1+nslen])
-    i += nslen+1
-    print(res[i])
-    assert res[i] == 0xC0
-    i += 1
-
-    offset = int(res[i])
-    
-    while res[offset] != 0x00:
-        partlen = int(res[offset])
-        partname = res[offset + 1: offset + 1 + partlen]
-        print(partname)
-        offset += partlen + 1
-
-    # print(res[offset:])
-    
-    # output = ''
-
-    
-    # while i < i + rdlength:
-    #     b = res[i]
-    #     if b == 0b11000000:
-    #         print("found pointer!")
-    #         i += 2
-    #     else: 
-    #         # process NS
-    #         nslen = int(b)
-    #         output += str(res[i+1:i+1+nslen]) + "."
-    #         i += i + 1 + nslen
-    #         print(output)
-    
-    #     i += 1
-    # print(output)
-        
-    # print('.'.join(str(b) for b in ip_addr))
-
-
-"""
-   - Parse name
-
-   - 
-
-"""
+    for _ in range(ancount):
+        i = parse_name(res, i)
