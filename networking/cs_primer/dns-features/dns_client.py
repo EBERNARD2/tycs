b4d76e40692bc96aa8bb523aff59495481745989
EBERNARD2 <carneybernard23@gmail.com>
2025-01-09 21:21:54 -0600

    basically done

--- networking/cs_primer/dns-features/dns_client.py
+++ networking/cs_primer/dns-features/dns_client.py
@@ -15,6 +15,8 @@ def parse_name(res, i):
     labels = []
     offsets = set()
     while True:
+        if i >= len(res):
+            break
         b = res[i]
         if b & 0b11000000:  # pointer
             if next_i is None:
@@ -32,11 +34,10 @@ def parse_name(res, i):
             i += b + 1
     return b'.'.join(labels), next_i
 
+def parse_ipv6(res, i):
+    pass
 
 if __name__ == '__main__':
-    if  len(sys.argv) == 1:
-        print("Usage: dns_client.py [flags] [domain | ip address]")
-        sys.exit(1)
     if "-x" in sys.argv:
         try:
             hostname = sys.argv[2]        
@@ -48,15 +49,14 @@ if __name__ == '__main__':
     else:
         hostname = sys.argv[1]
         try:
-            # match resource type to rtype flag. See RFC 1035 Section 3.2.2
             rtype = {
                 'A': 1,
                 'NS': 2,
                 'CNAME': 5,
-                'SOA': 16,
+                'SOA': 6,
                 'MX': 15,
                 'TXT': 16,
-                'AAAA' : 28, # See RFC 3596 section 2.1 to query IPV6 addresses https://datatracker.ietf.org/doc/html/rfc3596
+                'AAAA': 28
             }[sys.argv[2]]
         except Exception:
             rtype = 1
@@ -87,32 +87,56 @@ if __name__ == '__main__':
         res, sender = s.recvfrom(4096)
         if sender != GOOGLE_PUBLIC_DNS:
             continue
-        rxid, rflags, qdcount, ancount, _, _ = \
+        rxid, rflags, qdcount, ancount, nscount, arcount = \
             struct.unpack('!HHHHHH', res[:12])
         if rxid == xid:
             break
 
+
+    # Skip headers
     i = 12
+
+    # Skip question section
     name, i = parse_name(res, i)
-    print(ancount)
     i += 4  # skip qtype and qclass
+
+    print(ancount, nscount, arcount)
+    print(res[2:4])
+    # Answer Section
     for idx in range(ancount):
-        name, i = parse_name(res, i)
-        rtype, rclass, ttl, rdlength = struct.unpack('!HHIH', res[i:i+10])
-        if rtype == 1:
-            ip_addr = res[i+10:i+10+rdlength]
-            print('.'.join(str(b) for b in ip_addr))
-        elif rtype == 2 or rtype == 12:
-            name, i = parse_name(res, i+10)
-            print(name)
-        # else:
-        #     print(res)
+      name, i = parse_name(res, i)
+      rtype, rclass, ttl, rdlength = struct.unpack('!HHIH', res[i:i+10])
+      if rtype == 1:
+          ip_addr = res[i+10:i+10+rdlength]
+          print('.'.join(str(b) for b in ip_addr))
+      elif rtype in [2,6,12]:
+          name, i = parse_name(res, i+10)
+          print(name)
+      elif rtype == 15:
+          name, i = parse_name(res, i+12)
+          print(name)
+      elif rtype == 28:
+          labels = []
+          for b in range(0, 16):
+              labels.append(res[i+10+b])
+              print(res[i+10+b])
+          print('.'.join(labels))
+          # parse_ipv6()
+
+    # Name Server Authority section 
+    for idx in range(nscount):
+      name, i = parse_name(res, i)
+      rtype, rclass, ttl, rdlength = struct.unpack('!HHIH', res[i:i+10])
+
+      if rtype == 6: # if the rr is a start of authroitiy server
+        name, i = parse_name(res, i+10)
+        print(name)
 
 
-"""
-  AAAA - not sure about this one yet
-  CNAME - Render whatever is after header 
-  MX - Mail Server - If we are not processing the additional section then we can use the current logic
-  SOA - Need logic to parse MNAME RNAME respectively
-  TXT - We can just print everything after (same as CNAME)
-"""
+    if rtype == 16:
+      # rtype, rclass, ttl, rdlength = struct.unpack('!HHIH', res[i:i+10])
+      print(res[:])
+    
+        
+
+      
