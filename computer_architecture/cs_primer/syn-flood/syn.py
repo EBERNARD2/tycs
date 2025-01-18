c594731796a164ba515d891563f3e48f4ec87cc0
EBERNARD2 <carneybernard23@gmail.com>
2025-01-18 13:28:59 -0600

    complete

--- computer_architecture/cs_primer/syn-flood/syn.py
+++ computer_architecture/cs_primer/syn-flood/syn.py
@@ -1,22 +1,6 @@
 import struct
 
 
-
-"""
-  This file is little endian so we need to unpack the bytes in big endian format
-
-
-  - Parse pcap header
-  - parse per packet header
-    - parse link layer packet
-    - parse ip header
-    - parse tcp 
-
-  
-  use pcap-savefile man page
-
-"""
-
 with open("synflood.pcap", "rb") as file:
   """ 
     Reference man page pcap-savefile for pcap header scheme
@@ -34,6 +18,10 @@ with open("synflood.pcap", "rb") as file:
   # this implementation assumes we are using the loopback interface so we need to assert the link layer header is infact 0
   assert llh == 0
   count = 0
+
+  server_port = None
+  initiated_connections = 0
+  acked_connections= 0
   while True:
     pack_header =  file.read(16)
     if len(pack_header) == 0:
@@ -50,15 +38,28 @@ with open("synflood.pcap", "rb") as file:
     assert packet[0] == 2 
     version = packet[4] >> 4
     ihl = (packet[4] & 0b1111) << 2
-    print(packet[6:24])
     assert version == 4
     assert ihl == 20
+  
+    src_port = struct.unpack("!H",packet[24:26])[0]
+    dest_port = struct.unpack("!H",packet[26:28])[0]
 
+    syn_bit = packet[34] & 0x02 >> 1
+    ack_bit = packet[34] & 0x10 >> 4
 
+    if dest_port == 80 and syn_bit:
+      initiated_connections += 1
 
-    
+    if src_port == 80 and ack_bit:
+      acked_connections += 1
+    print(f"Src: {src_port} -> Dest: {dest_port}")
+  
+
 
   print(f"{count} packets parsed")
+  print(f"{initiated_connections} connections to server")
+  print(f"{acked_connections} acks")
+  print(f"{(acked_connections/ initiated_connections) * 100}% of connections were properly ACKED")
 
 
 
