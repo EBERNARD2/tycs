f224d3feb54ac262ba9c2ecb91da74088c5192e1
EBERNARD2 <carneybernard23@gmail.com>
2025-01-17 22:44:07 -0600

    need to parse tcp header now

--- computer_architecture/cs_primer/syn-flood/syn.py
+++ computer_architecture/cs_primer/syn-flood/syn.py
@@ -33,19 +33,34 @@ with open("synflood.pcap", "rb") as file:
 
   # this implementation assumes we are using the loopback interface so we need to assert the link layer header is infact 0
   assert llh == 0
-  t_stamp_secs, t_stamp_micro_secs, pack_len, pack_data_len = struct.unpack("<IIII", file.read(16))
+  count = 0
+  while True:
+    pack_header =  file.read(16)
+    if len(pack_header) == 0:
+      break
+    count += 1
+    # see pcap --savefile man page
+    _, _, pack_len, pack_data_len = struct.unpack("<IIII", pack_header)
+    assert pack_len == pack_data_len
+    packet = file.read(pack_len)
+    #parse packet headers
+    """
+      Refer to https://en.wikipedia.org/wiki/IPv4#Header
+    """
+    assert packet[0] == 2 
+    version = packet[4] >> 4
+    ihl = (packet[4] & 0b1111) << 2
+    print(packet[6:24])
+    assert version == 4
+    assert ihl == 20
 
-  assert struct.unpack("<I",file.read(4))[0] == 2 # confirm that packet is a ipv4 packet
-  print(pack_data_len)
 
-  """
-    Refer to https://en.wikipedia.org/wiki/IPv4#Header
-  """
-  b = struct.unpack("<B", file.read(1))[0]
- 
-  # parse header
 
-  print(b & 0b11110000)
+    
+
+  print(f"{count} packets parsed")
+
+
 
 
 
